9c112307dee9bc43f571cde2cc6f15f202cac591
TZajas <20278748@studentmail.ul.ie>
2022-04-28 20:02:59 +0100

    Lab week12 Finished

---
+++ week11/fill.c
@@ -0,0 +1,81 @@
+
+#include <stdlib.h>
+#include <GL/glut.h>
+
+const double M = 1;
+const double N = 1.5;
+
+GLfloat vertices[][3] = {{0.0,M},{-1*M,0.0},
+			{0.0,-2*M}, {2*M,0.0}};
+
+GLfloat colors[][3] = {{1.0,0.0,0.0},{0.0,1.0,0.0},
+		       {0.0,0.0,1.0}, {0.0,0.0,0.0}};
+
+void polygon(int a, int b, int c , int d)
+{
+
+ 	glBegin(GL_POLYGON);
+		glColor3fv(colors[a]);
+		glVertex2fv(vertices[a]);
+		glColor3fv(colors[b]);
+		glVertex2fv(vertices[b]);
+		glColor3fv(colors[c]);
+		glVertex2fv(vertices[c]);
+		glColor3fv(colors[d]);
+		glVertex2fv(vertices[d]);
+	glEnd();
+																										}
+
+void colorPoly(void)
+{
+
+	polygon(0,1,2,3);
+
+}
+
+void display(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+
+ 	colorPoly();
+
+ 	glFlush();
+	glutSwapBuffers();
+
+}
+
+void myReshape(int w, int h)
+{
+
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if (w <= h)
+    	glOrtho(-2.0,2.0,-2.0*(GLfloat)h/(GLfloat)w,
+            2.0*(GLfloat)h/(GLfloat)w,-10.0,10.0);
+    else
+        glOrtho(-2.0 * (GLfloat) w / (GLfloat) h,
+            2.0 * (GLfloat) w / (GLfloat) h, -2.0, 2.0, -10.0, 10.0);
+
+    glMatrixMode(GL_MODELVIEW);
+
+}
+
+int main(int argc, char **argv)
+{
+
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(650, 650);
+    glutCreateWindow("Coloured");
+
+    glutReshapeFunc(myReshape);
+    glutDisplayFunc(display);
+	glEnable(GL_DEPTH_TEST);
+
+    glutMainLoop();
+
+}
